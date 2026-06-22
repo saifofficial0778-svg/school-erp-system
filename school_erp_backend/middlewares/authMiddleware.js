c6086e81bf2456c4340e21140e0971f2008a4a7e
Mohd Saif <saifofficial0778@gmail.com>
2026-06-22 16:57:21 +0530

    middleware

--- school_erp_backend/middlewares/authMiddleware.js
+++ school_erp_backend/middlewares/authMiddleware.js
@@ -1,8 +1,8 @@
 const jwt =require('jsonwebtoken')
 
 const verifyToken=(req,res,next)=>{
-    const authHeader=req.headers['authentication'];
-    const token=authHeader && authHeader.split('')[1];
+    const authHeader=req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
     if(!token){
         return res.status(401).json({message:"Access Denied: Token missing!"})
